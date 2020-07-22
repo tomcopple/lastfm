3a1efb56889572f4cca1534d9fc304e302402ba1
tomcopple <tomcopple@gmail.com>
2020-07-22 10:07:17 +0100

    Saving treemap to dropbox

--- scripts/getPlexRatings.R
+++ scripts/getPlexRatings.R
@@ -1,16 +1,19 @@
 ## Query Plex API?
 
-getPlexAlbumRatings <- function(refresh = FALSE, write = FALSE) {
+getPlexAlbumRatings <- function(refresh = FALSE, write = FALSE, printTree = FALSE) {
     library(tidyverse);library(httr);library(lubridate);library(rdrop2);library(treemapify)
     token <- '2CdDkLKF5xY27xxuxHB5'
     
     ## Music is section 5, need to search '10' to get all tracks
     if(refresh) {
+        
+        print("Downloading plex data")
         allTracks <- content(GET("http://192.168.1.99:32400/library/sections/5/search?type=10", 
                                  add_headers("X-Plex-Token" = token))) %>% 
             magrittr::extract2('MediaContainer') %>% 
             magrittr::extract2('Metadata')
         
+        print("Cleaning data")
         plex <- allTracks %>% 
             map_df(function(x) {
                 as_tibble(x) %>% 
@@ -18,26 +21,31 @@ getPlexAlbumRatings <- function(refresh = FALSE, write = FALSE) {
             }) %>% 
             select(albumArtist = grandparentTitle, artist = originalTitle, album = parentTitle, track = title, rating = userRating)
         
-        treemap <- plex %>% 
+        .GlobalEnv$plex <- plex
+        
+        print("Producing treemap")
+        treemapData <- plex %>% 
             select(album, rating) %>% 
             mutate(rating = replace_na(rating, 0),
                    rating = round(rating / 2) * 2,
                    n = 1) %>% 
-            mutate(rating = forcats::as_factor(rating)) %>% 
-            ggplot(aes(area = n, fill = rating, subgroup = album)) +
+            mutate(rating = forcats::as_factor(rating))  
+        treeMap <- ggplot(treemapData, aes(area = n, fill = rating, subgroup = album)) +
                 geom_treemap() +
                 geom_treemap_subgroup_border(size = 1) +
                 scale_fill_brewer()
-        print(treemap)
+        if(printTree) { print(treemap) }
+        treeFilename <- here::here('tempData', str_c(
+            str_replace_all(lubridate::now(), pattern = "[:\\s]", "-"), '-treemap.png'))
         ggsave(plot = treemap,
-               filename = here::here('tempData', str_c(lubridate::today(), '-treemap.png')),
+               filename = treeFilename,
                width = 10.5, height = 9.14, units = "in")
-        rdrop2::drop_upload(file = here::here('tempData', str_c(lubridate::today(), '-treemap.png')),
+        rdrop2::drop_upload(file = treeFilename,
                             path = 'R/lastfm/')
         
         
         ## Save a copy of albums/number of tracks?
-        albumCount <- plex
+        albumCount <- plex %>% 
             count(albumArtist, album)
         
         ## Save tracks that have a rating
@@ -58,6 +66,7 @@ getPlexAlbumRatings <- function(refresh = FALSE, write = FALSE) {
     
     ## Save one copy date-stamped today, and another master copy
     if(write) {
+        print("Saving backup copy of data")
         write_csv(ratedDF, here::here('tempData', 
                                       str_c(lubridate::today(), "-plexRatings.csv")))
         rdrop2::drop_upload(file = here::here('tempData', 
