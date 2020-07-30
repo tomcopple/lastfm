48db2f23c9f1b9d5f3a5727c133748cdf6c84aa1
Tom Copple <tomcopple@gmail.com>
2020-07-30 11:22:49 +0100

    adding refresh option to plex

--- scripts/getPlex.R
+++ scripts/getPlex.R
@@ -1,28 +1,40 @@
 ## Get all Plex info
 
-getPlex <- function() {
+getPlex <- function(refresh = FALSE) {
     
     library(tidyverse);library(httr);library(lubridate);library(rdrop2)
-    token <- '2CdDkLKF5xY27xxuxHB5'
-    allTracks <- content(GET("http://192.168.1.99:32400/library/sections/5/search?type=10", 
-                             add_headers("X-Plex-Token" = token))) %>% 
-        magrittr::extract2('MediaContainer') %>% 
-        magrittr::extract2('Metadata')
     
-    ratedDF <- allTracks %>% 
-        map_df(function(x) {
-            as_tibble(x) %>% 
-                select(any_of(c("grandparentTitle", "originalTitle", 
-                                "parentTitle", "title", "userRating", "ratingKey")))
-        }) %>% 
-        select(albumArtist = grandparentTitle, 
-               artist = originalTitle, 
-               album = parentTitle, 
-               track = title, 
-               rating = userRating,
-               key = ratingKey)
+    if (refresh) {
+        token <- '2CdDkLKF5xY27xxuxHB5'
+        allTracks <- content(GET("http://192.168.1.99:32400/library/sections/5/search?type=10", 
+                                 add_headers("X-Plex-Token" = token))) %>% 
+            magrittr::extract2('MediaContainer') %>% 
+            magrittr::extract2('Metadata')
+        
+        ratedDF <- allTracks %>% 
+            map_df(function(x) {
+                as_tibble(x) %>% 
+                    select(any_of(c("grandparentTitle", "originalTitle", 
+                                    "parentTitle", "title", "userRating", "ratingKey")))
+            }) %>% 
+            select(albumArtist = grandparentTitle, 
+                   artist = originalTitle, 
+                   album = parentTitle, 
+                   track = title, 
+                   rating = userRating,
+                   key = ratingKey)
+        
+        write.csv(ratedDF, file = "plexDB.csv", 
+                  row.names = FALSE, fileEncoding = "UTF-8")
+        rdrop2::drop_upload(file = "plexDB.csv", path = "R/lastfm")
+        
+        return(ratedDF)    
+    } else {
+        
+        ratedDF <- rdrop2::drop_read_csv(file = "R/lastfm/plexDB.csv")
+        return(ratedDF)
+        
+    }
     
-    return(ratedDF)
     
 }
-plex <- getPlex()
