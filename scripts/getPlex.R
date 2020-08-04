2f856cbeb4c9c5c7b80b688ddeed077e5c083706
tomcopple <tomcopple@gmail.com>
2020-08-04 15:06:30 +0100

    hopefully simplified getPlex

--- scripts/getPlex.R
+++ scripts/getPlex.R
@@ -2,52 +2,32 @@
 
 getPlex <- function(refresh = FALSE) {
     
-    library(tidyverse);library(httr);library(lubridate);library(rdrop2)
+    library(tidyverse);library(httr);library(lubridate);library(rdrop2);library(jsonlite)
     
-<<<<<<< HEAD
-    numberOfTracks <- length(allTracks)
-    tenPC <- round(numberOfTracks /10, 0)*10
-    
-    plex <- allTracks %>% 
-        map_df(function(x) {
-            as_tibble(x) %>% 
-                select(any_of(c("grandparentTitle", "originalTitle", 
-                                "parentTitle", "title", "userRating", "ratingKey")))
-        }) %>% 
-        select(albumArtist = grandparentTitle, 
-               artist = originalTitle, 
-               album = parentTitle, 
-               track = title, 
-               rating = userRating,
-               key = ratingKey)
-    
-    return(plex)
-=======
     if (refresh) {
         token <- '2CdDkLKF5xY27xxuxHB5'
         allTracks <- content(GET("http://192.168.1.99:32400/library/sections/5/search?type=10", 
-                                 add_headers("X-Plex-Token" = token))) %>% 
+                                 add_headers("X-Plex-Token" = token)), type = 'text') %>% 
+            jsonlite::fromJSON() %>% 
             magrittr::extract2('MediaContainer') %>% 
-            magrittr::extract2('Metadata')
-        
-        ratedDF <- allTracks %>% 
-            map_df(function(x) {
-                as_tibble(x) %>% 
-                    select(any_of(c("grandparentTitle", "originalTitle", 
-                                    "parentTitle", "title", "userRating", "ratingKey")))
-            }) %>% 
-            select(albumArtist = grandparentTitle, 
-                   artist = originalTitle, 
-                   album = parentTitle, 
-                   track = title, 
-                   rating = userRating,
-                   key = ratingKey)
+            magrittr::extract2('Metadata')%>% 
+            select(
+                albumArtist = grandparentTitle, 
+                   artist      = originalTitle, 
+                   album       = parentTitle, 
+                   track       = title, 
+                   rating      = userRating,
+                   key         = ratingKey,
+                   artistKey   = grandparentRatingKey,
+                   albumKey    = parentRatingKey
+            )
         
         write.csv(ratedDF, file = "plexDB.csv", 
                   row.names = FALSE, fileEncoding = "UTF-8")
         rdrop2::drop_upload(file = "plexDB.csv", path = "R/lastfm")
         
         return(ratedDF)    
+        
     } else {
         
         ratedDF <- rdrop2::drop_read_csv(file = "R/lastfm/plexDB.csv") %>% 
@@ -55,7 +35,4 @@ getPlex <- function(refresh = FALSE) {
         return(ratedDF)
         
     }
-    
->>>>>>> fbb320385a5f5d60e420751636b1935b06db6591
-    
 }
