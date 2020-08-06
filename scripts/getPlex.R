0c931bc4878d0ab73df45841a88139bbfa1814aa
Tom Copple <tomcopple@gmail.com>
2020-08-06 13:58:22 +0100

    Adding trackNum discNum

--- scripts/getPlex.R
+++ scripts/getPlex.R
@@ -6,34 +6,38 @@ getPlex <- function(refresh = FALSE) {
     
     if (refresh) {
         token <- '2CdDkLKF5xY27xxuxHB5'
-        allTracks <- content(GET("http://192.168.1.99:32400/library/sections/5/search?type=10", 
+        plexRaw <- content(GET("http://192.168.1.99:32400/library/sections/5/search?type=10", 
                                  add_headers("X-Plex-Token" = token)), type = 'text') %>% 
             jsonlite::fromJSON() %>% 
             magrittr::extract2('MediaContainer') %>% 
-            magrittr::extract2('Metadata')%>% 
-            select(
-                albumArtist = grandparentTitle, 
+            magrittr::extract2('Metadata')
+        
+        plex <- plexRaw %>% select(
+                albumArtist    = grandparentTitle, 
                    artist      = originalTitle, 
                    album       = parentTitle, 
                    track       = title, 
                    rating      = userRating,
                    key         = ratingKey,
                    artistKey   = grandparentRatingKey,
-                   albumKey    = parentRatingKey
+                   albumKey    = parentRatingKey,
+                trackNum       = index,
+                discNum        = parentIndex
             )
         
-        write.csv(allTracks, file = here::here('tempData', 'plexDB.csv'), 
+        write.csv(plex, file = here::here('tempData', 'plexDB.csv'), 
                   row.names = FALSE, fileEncoding = "UTF-8")
         rdrop2::drop_upload(file = here::here('tempData', "plexDB.csv"), 
                                               path = "R/lastfm")
         
-        return(allTracks)    
         
     } else {
         
-        allTracks <- rdrop2::drop_read_csv(file = "R/lastfm/plexDB.csv") %>% 
+        plex <- rdrop2::drop_read_csv(file = "R/lastfm/plexDB.csv") %>% 
             as_tibble()
-        return(allTracks)
         
     }
+    
+    return(plex)
+    
 }
