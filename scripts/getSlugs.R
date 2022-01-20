924274a6ca398485da817fada39b597a55c64e00
tomcopple <tomcopple@gmail.com>
2022-01-20 10:16:50 +0000

    Removing feats from slugs

--- scripts/getSlugs.R
+++ scripts/getSlugs.R
@@ -5,25 +5,20 @@ getSlugs <- function(text) {
     
     library(stringr)
     text1 <- text %>% 
+        ## Remove any accents, cedillas etc (need to do this before lower case)
+        iconv(from = 'UTF-8', to = "ASCII//TRANSLIT") %>% 
         str_to_lower() %>% 
         str_remove_all(pattern = "[[:punct:]]") %>% 
         str_replace_all(pattern = "&", "and") %>% 
         str_replace_all(pattern = "\\s{2,}", replacement = " ") %>% 
-        str_trim(side = 'both') %>% 
+        ## Remove anything in brackets e.g. (remastered) etc
         str_remove_all(pattern = "\\s\\(.*") %>% 
+        ## and remove any "feat."
+        str_remove_all(pattern = "feat.*") %>% 
+        str_trim(side = 'both') %>% 
         str_replace_all(pattern = " ", replacement = "-")
     
     return(text1)
     
 }
 
-getSlugs <- function(x) {
-    x1 <- stringr::str_to_lower(x)
-    x2 <- stringr::str_replace_all(x1, "&", "and")
-    x3 <- stringr::str_remove_all(x2, "[:punct:]")
-    x4 <- stringr::str_replace_all(x3, "\\s{2,}", " ")
-    x5 <- stringr::str_trim(x4, side = "both")
-    ## Remove anything in brackets, e.g. (remastered) etc
-    x6 <- stringr::str_remove_all(x5, "\\s\\(.*")
-    return(x6)
-}
