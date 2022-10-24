5f1bfc20a24b22b0f52bfc2667ca3f64e226c3e0
unknown <tomcopple@gmail.com>
2022-10-24 16:59:49 +0100

    Removing a couple more

--- scripts/new22.R
+++ scripts/new22.R
@@ -20,5 +20,8 @@ playedBefore <- tracks %>%
     arrange(desc(n))
 
 anti_join(played22, playedBefore, by = 'album') %>% 
-    filter(str_detect(album, 'Motown Singles', negate = T)) %>% 
+    filter(str_detect(album, 'Motown Singles', negate = T),
+           str_detect(album, 'Classical Lullabies', negate = T),
+           str_detect(album, 'Highway Strip', negate = T),
+           str_detect(album, 'Superworm', negate = T)) %>% 
     arrange(n)
